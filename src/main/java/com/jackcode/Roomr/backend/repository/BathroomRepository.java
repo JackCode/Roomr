b39d3dfb0ab207946256eead47987eabaf67663e
J Weber <63063256+epicrijks@users.noreply.github.com>
2021-01-11 12:35:14 -0500

    Create Repository

---
+++ src/main/java/com/jackcode/Roomr/backend/repository/BathroomRepository.java
@@ -0,0 +1,7 @@
+package com.jackcode.Roomr.backend.repository;
+
+import com.jackcode.Roomr.backend.model.Bathroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BathroomRepository extends JpaRepository<Bathroom, Long> {
+}
