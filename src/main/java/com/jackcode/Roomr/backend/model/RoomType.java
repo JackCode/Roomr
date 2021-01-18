d8c1ab85154e560c32e32b80a8612f883747a59b
J Weber <63063256+epicrijks@users.noreply.github.com>
2021-01-17 23:41:43 -0500

    fix K1W Description

--- src/main/java/com/jackcode/Roomr/backend/model/RoomType.java
+++ src/main/java/com/jackcode/Roomr/backend/model/RoomType.java
@@ -3,7 +3,7 @@ package com.jackcode.Roomr.backend.model;
 public enum RoomType {
     K1("Standard 1 King Bed"),
     K1T("Premium 1 King Bed"),
-    K1W("Premium 1 King Bed w/ Whirlpool Tub"),
+    K1W("1 King Bed w/ Whirlpool Tub"),
     Q2("Standard 2 Queen Beds"),
     Q2T("Premium 2 Queen Beds"),
     K1RRU1("1 King Bed - Hannaford Suite"),
